bc5e7f9c339a9e6ff60808293493f0335a9041e1
i-abramson19 <abramsonisaiah@gmail.com>
2019-05-13 16:29:06 -0700

    usser

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,48 +1,45 @@
-import axios from 'axios';
+import axios from "axios";
+import { createMessage, returnErrors } from "./messages";
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
-// Get leads
+// GET LEADS
 export const getLeads = () => dispatch => {
-    axios.get('/api/leads/')
-        .then(res => {
-            dispatch({
-                type: GET_LEADS,
-                payload: res.data
-            });
-        })
-        .catch(err => console.log(err));
+  axios
+    .get('/api/leads/')
+    .then(res => {
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-// Delete lead
-export const deleteLead = (id) => dispatch => {
-    axios.delete(`/api/leads/${id}/`)
-        .then(res => {
-            dispatch({
-                type: DELETE_LEAD,
-                payload: id
-            });
-        })
-        .catch(err => console.log(err));
+// DELETE LEAD
+export const deleteLead = id => dispatch => {
+  axios
+    .delete(`/api/leads/${id}`)
+    .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.log(err));
 };
 
-// Add lead
-export const addLead = (lead) => dispatch => {
-    axios.post("/api/leads/", lead)
-        .then(res => {
-            dispatch({
-                type: ADD_LEAD,
-                payload: res.data
-            });
-        })
-        .catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            };
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            });
-        });
+// ADD LEAD
+export const addLead = lead => dispatch => {
+  axios
+    .post("/api/leads/", lead)
+    .then(res => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    })
+    .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+    related_name="leads", blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
