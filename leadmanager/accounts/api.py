83d5abbed8e22b29e66a053b43534f96e6ea025b
i-abramson19 <abramsonisaiah@gmail.com>
2019-05-17 18:12:38 -0700

    update

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
-
+#
 # Register API
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
